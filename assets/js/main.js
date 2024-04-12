b91b387aeb82aac590408f369f1b517cbec39ac7
JeanMarc-RAJAONARIVELONA <jeanmarc.rajaonarivelona01@gmail.com>
2024-04-12 23:07:22 +0300

    feat: create success message when reservation done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 const navbar = document.querySelector("nav");
-
-console.log(navbar);
+const reservationBtn = document.querySelector(".reservation-button");
+const successreservation = document.querySelector("#successReservation");
 //redirect into login page
 const signout = document.querySelector(".sign-out");
 signout.addEventListener("click", () => {
@@ -15,3 +15,12 @@ window.addEventListener("scroll", () => {
     navbar.classList.remove("nav-scrolled");
   }
 });
+//show reservation message success
+function reservation() {
+  successreservation.classList.remove("reservation-hidden");
+  successreservation.classList.add("reservation-succes");
+  setTimeout(() => {
+    successreservation.classList.add("reservation-hidden");
+    successreservation.classList.remove("reservation-succes");
+  }, 1500);
+}
