71ff12dc7ed2cd06c775f13bccb07077b30e30d5
JeanMarc-RAJAONARIVELONA <jeanmarc.rajaonarivelona01@gmail.com>
2024-04-12 22:10:30 +0300

    feat: change navbar position with js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,17 @@
+const navbar = document.querySelector("nav");
+
+console.log(navbar);
+//redirect into login page
 const signout = document.querySelector(".sign-out");
 signout.addEventListener("click", () => {
   window.location.replace("login.html");
+});
+
+//change navbar position
+window.addEventListener("scroll", () => {
+  if (document.documentElement.scrollTop > 10) {
+    navbar.classList.add("nav-scrolled");
+  } else {
+    navbar.classList.remove("nav-scrolled");
+  }
 });
